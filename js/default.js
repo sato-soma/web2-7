8cc97e01f2bf4336913cef28a02fec41bdd94100
サトウ　ソウマ <38420480+sato-soma@users.noreply.github.com>
2018-11-19 18:49:04 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,15 @@
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('数あてゲーム。０～２の数字を入力してください！'));
+
+var message;
+if(answer === number){
+  message = 'あたり!';
+}else if(answer < number){
+  message = '残念!大きいです';
+}else if(answer > number){
+  message = '残念！小さいです';
+}else{
+  message = '0~3の数字を入力してください';
+}
+
+document.getElementById('choice').textContent = message;
